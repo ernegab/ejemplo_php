6968f9e2b08e9de4f4202164ffd8fc49df060473
ernegab <84035916+ernegab@users.noreply.github.com>
2021-05-12 19:33:57 -0300

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,4 +1,4 @@
-<div class="jumbotron bg-cover" style="background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url(Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png)">
+<div class="jumbotron bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url(Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png)">
 	<h1>Prueba PHP</h1>
 	<!-- <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> -->
 </div>
