3a744da29463c7aeab996a524fe3a995152628df
ernegab <84035916+ernegab@users.noreply.github.com>
2021-05-12 18:23:25 -0300

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,3 +1,4 @@
 <div class="jumbotron">
 	<h1>Hello there!</h1>
+	<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Copa_EuroAmericana_-_Nacional_vs._Atl%C3%A9tico_Madrid_004.png/1280px-Copa_EuroAmericana_-_Nacional_vs._Atl%C3%A9tico_Madrid_004.png' />
 </div>
