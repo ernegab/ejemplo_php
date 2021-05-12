256213a94eced66c2bdd9a7968ac75973d920f5b
ernegab <84035916+ernegab@users.noreply.github.com>
2021-05-12 18:50:43 -0300

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,4 +1,4 @@
-<div class="jumbotron text-white jumbotron-image shadow" style="background-image: url(https://images.unsplash.com/photo-1552152974-19b9caf99137?fit=crop&w=1350&q=80);">
+<div class="jumbotron text-white jumbotron-image shadow" style="background-image: url(Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png?fit=crop&w=1350&q=80);">
 	<h1>Hello there!</h1>
 	<!-- <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> -->
 </div>
