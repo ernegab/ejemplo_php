fe6bc6ba0d9d0cffebce94fe683bb048fd144a09
ernegab <84035916+ernegab@users.noreply.github.com>
2021-05-12 19:09:09 -0300

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,6 +1,6 @@
 <div class="jumbotron">
 	<div class="container">
-        <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> 
+		<img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> 
         </div>
 	<!-- <h1>Hello there!</h1> -->
 	<!-- <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> -->
