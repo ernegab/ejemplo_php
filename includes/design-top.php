dab0b1a24f3777c100d63f8cb460c2e7a17196ca
ernegab <84035916+ernegab@users.noreply.github.com>
2021-05-12 19:08:26 -0300

    Update design-top.php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,4 +1,7 @@
-<div class="jumbotron text-white jumbotron-image shadow" style="background-image: url(Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png); height:100vh;">
+<div class="jumbotron">
+	<div class="container">
+        <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> 
+        </div>
 	<!-- <h1>Hello there!</h1> -->
 	<!-- <img src='Copa_EuroAmericana_-_Nacional_vs._Atlético_Madrid_004.png' /> -->
 </div>
